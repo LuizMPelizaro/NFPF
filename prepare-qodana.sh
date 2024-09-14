0f92d846ee737e903b4d58b576f012b71f262072
LMP <luiz.pelizaro127@hotmail.com>
2024-09-14 11:45:41 -0300

    remove qodana

--- prepare-qodana.sh
+++
@@ -1,9 +0,0 @@
-  # setup python
-
-  python3 -m venv /data/cache/venv
-  source /data/cache/venv/bin/activate
-  python3 -m pip install -r /data/project/requirements.txt
-
-  # remove idea directory (No Python interpreter configured for the project)
-  # https://github.com/JetBrains/Qodana/discussions/134#discussioncomment-4329981
-  rm -rf .idea
